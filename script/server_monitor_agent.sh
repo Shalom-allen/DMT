876d2077834b3c9ac7f5635eb5201bb21a7dea4d
min950103 <min950103@kakao.com>
2024-01-09 15:54:57 +0900

    메모리 항목 추가중 및 기능 개선

--- script/server_monitor_agent.sh
+++ script/server_monitor_agent.sh
@@ -8,12 +8,16 @@
 # 설명   : 에이전트
 ###############################################################
 
-#비밀번호 불러오기
+# 비밀번호 불러오기
 N_DB_PW=$(cat /test/db_info.conf | grep -a -1 MONITOR_MAIN_DB_PW | cut -d '=' -f2)
 DB_PW="${N_DB_PW:1:10}"
 
-#서버 CPU 모니터링
-CPU_VALUE=$(top -bn 1 | awk '{ print $9}' | awk 'NR>=8{print}' | awk '{total_size += $9} END {print total_size;}')
+# 서버 CPU 모니터링
+CPU_VALUE=$(top -bn 1 | awk '{ print $9}' | awk 'NR>=8{print}' | awk '{total_size += $1;} END {print total_size;}')
+# 서버 MEMORY 모니터링
+MEMORY_USAGE=$(top -bn 1 | awk '{print $10}' | awk 'NR>=8{print}' | awk '{total_size += $1;} END {print total_size;}')
 
-#CPU값 insert
+
+# CPU값 insert
 mariadb -u root -p$DB_PW TEST01 -e "insert into TBL_CPU_CHECK values (sysdate(),$CPU_VALUE);"
+
