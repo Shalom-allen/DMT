c7fa1cc7c98a42c8b45587d3d28bb3a6c468fcdc
min950103 <min950103@kakao.com>
2024-01-18 17:20:52 +0900

    수정

---
+++ script/Windows/test.ps1
@@ -0,0 +1,15 @@
+# Windows Power Shell
+
+################################################
+# 작성자    : 유민상
+# 작성일    : 2024-01-18
+# 수정자    : 
+# 수정일    : 
+# 설명      : 윈도우&MSSQL 모니터링 에이전트
+###############################################
+cd C:
+
+while (1 -eq 1) {
+        . 'D:\04.study\DMT\script\Windows\server_monitor_agent.ps1'
+        #start-sleep -seconds 5
+}
