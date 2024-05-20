34cb3c90c342b11b2f325b274fa0942577e5a05b
min950103 <min950103@kakao.com>
2024-05-20 10:55:04 +0900

    2024-05-20

--- script/Linux/create_admin_db.sh
+++ script/Linux/create_admin_db.sh
@@ -16,7 +16,7 @@ read pw
 stty echo
 echo "======================================================================"
 
-RESULT=$(maraidb -u root -p$pw -e "select 0 as 'result'")
+RESULT=$(mariadb -u root -p$pw -e "select 0 as 'result'")
 
 if [ "${RESULT:6:7}" -eq 0 ]; then
 	echo "접속가능"
@@ -30,12 +30,13 @@ echo "======================================================================"
 echo "STM DB 생성중..."
 mariadb -u root -p$pw -e "create database STM"
 mariadb -u root -p$pw -e "show databases"
+echo "STM DB 생성완료"
 echo "======================================================================"
 
 # Table 생성
 echo "======================================================================"
-mariadb -u root -p$pw -e "show tables"
-mariadb -u root -p$pw STM -e "CREATE TABLE ADM_USER (CD_ILJA DATETIME NOT NULL COMMENT '일자', CD_USERID VARCHAR(32) NOT NULL COMMENT '유저아이디', NM_PASSWD VARCHAR(20) NOT NULL COMMENT '비밀번호', NO_XPASS TINYINT UNSIGNED NOT NULL COMMENT '비밀번호틀린횟수', NM_ROLE VARCHAR(10) NOT NULL COMMENT	'권한명', NM_ROLE_DETAIL VARCHAR(500) NOT NULL COMMENT '권한_상세내용', NM_IP_1 VARCHAR(15) COMMENT '아이피1', NM_IP_2 VARCHAR(15) COMMENT '아이피2', DS_USE ENUM('Y','N') NOT NULL COMMENT '사용구분', SD_UPDATE DATETIME COMMENT '업데이트일자', CONSTRAINT PRIMARY KEY (CD_ILJA desc, CD_USERID ASC)) COMMENT 'STM_계정리스트';"
-mariadb -u root -p$pw STM -e "CREATE TABLE ADM_USER_LOG (CD_ILJA DATETIME NOT NULL COMMENT '일자', CD_USERID	VARCHAR(32) NOT NULL COMMENT '유저아이디', NM_DETAIL	VARCHAR(400) COMMENT '상세내역', NM_IP VARCHAR(15) COMMENT '접속IP', CONSTRAINT PRIMARY KEY (CD_ILJA desc)) COMMENT 'STM_계정로그';"
-mariadb -u root -p$pw -e "show tables"
+mariadb -u root -p$pw STM -e "show tables"
+mariadb -u root -p$pw STM -e "CREATE TABLE ADM_USER (CD_ILJA DATETIME NOT NULL COMMENT '일자', CD_USERID VARCHAR(32) NOT NULL COMMENT '유저아이디', NM_PASSWD VARCHAR(20) NOT NULL COMMENT '비밀번호', NO_XPASS TINYINT UNSIGNED NOT NULL COMMENT '비밀번호틀린횟수', NM_ROLE VARCHAR(10) NOT NULL COMMENT	'권한명', NM_IP_1 VARCHAR(15) COMMENT '아이피1', NM_IP_2 VARCHAR(15) COMMENT '아이피2', DS_USE ENUM('Y','N') NOT NULL COMMENT '사용구분', SD_UPDATE DATETIME COMMENT '업데이트일자', CONSTRAINT PRIMARY KEY (CD_ILJA desc, CD_USERID ASC)) COMMENT 'STM_계정리스트';"
+mariadb -u root -p$pw STM -e "CREATE TABLE ADM_USER_LOG (CD_ILJA DATETIME NOT NULL COMMENT '일자', CD_USERID VARCHAR(32) NOT NULL COMMENT '유저아이디', NM_ROLE VARCHAR(10) NOT NULL COMMENT '권한명', NM_DETAIL VARCHAR(400) COMMENT '상세내역', NM_IP VARCHAR(15) COMMENT '접속IP', CONSTRAINT PRIMARY KEY (CD_ILJA desc)) COMMENT 'STM_계정로그';"
+mariadb -u root -p$pw STM -e "show tables"
 echo "======================================================================"
